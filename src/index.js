b2f6be4d00d05dfbfd4f3ff722556d2c1311da01
Jose Rosas <josejrrosas@yahoo.com>
2025-06-01 20:51:52 -0500

    imported modules to index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import displayHome from "./home"
-console.log("Restaurant Page Webpack is working!");
+import displayMenu from "./menu"
+import displayAbout from "./about"
+
 displayHome();
 
+const homeBtn = document.getElementById("home-btn");
+const menuBtn = document.getElementById("menu-btn");
+const aboutBtn = document.getElementById("about-btn");
 
+homeBtn.addEventListener('click', () => {
+
+})
+
+menuBtn.addEventListener('click', () => {
+    
+})
+
+aboutBtn.addEventListener('click', () => {
+    
+})
